de90a9f35eb2a12117d21c1868ba21cbd5289ec6
Monika Łasak <monika.k.lasak@gmail.com>
2018-05-05 12:10:25 +0200

    Change scripts

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ var Itemsbuton = document.getElementsByClassName('buton');
 console.log(Itemsbuton);
 
 for (var i = Itemsbuton.length - 1; i >= 0; i--) {
-	infotext.innerText = buton;
+	infotext.innerText = 'Hello';
 }
